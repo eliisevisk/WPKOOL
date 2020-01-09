716fd2b2b9fea258035fa0ab64366f746563529e
Eliise <eliise.visk@khk.ee>
2020-01-09 11:39:04 +0200

    modified plugin `My Plugin` version 0.1 (after activation of `Raining Hearts` version Modified)

--- wp-content/plugins/my-plugin/my-plugin.php
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -2,8 +2,31 @@
 /**
  * Plugin Name: My Plugin
  * Plugin URI: viskeliise.ikt.khk.ee/
- * Description: My Plugin boyee
+ * Description: My Plugin
  * Version: 0.1
  * Author: Eliise Visk
  * Author URI: viskeliise.ikt.khk.ee/
  **/
+
+
+//global variables
+
+
+//fns
+
+
+// function my_plugin_test() {
+    
+// 	echo 'Hello World'; exit;
+// }
+// add_action('admin_head', 'my_plugin_test');
+
+// function dh_modify_read_more_link() {
+//     return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
+// }
+// add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
+
+
+
+//includes
+include('./includes/scripts.php');
