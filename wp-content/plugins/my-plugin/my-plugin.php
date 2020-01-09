fe724fb3ee8a1435e61527b8dc58c2f5982b7cb3
Eliise <eliise.visk@khk.ee>
2020-01-09 10:52:57 +0200

    modified plugin `My Plugin` version 0.1 (after activation of `My Plugin` version 0.1)

---
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: My Plugin
+ * Plugin URI: viskeliise.ikt.khk.ee/
+ * Description: My Plugin boyee
+ * Version: 0.1
+ * Author: Eliise Visk
+ * Author URI: viskeliise.ikt.khk.ee/
+ **/
